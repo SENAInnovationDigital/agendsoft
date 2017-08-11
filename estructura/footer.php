231076856b32f200cdd65646b9fad31a000a6be9
DESKTOP-V9CGA3U\Edwar Prieto <eduar.ar1996@hotmail.com>
2017-08-10 23:36:36 -0500

    Footer con PHP
    Se creó el footer del aplicativo yse incluyó con PHP

---
+++ estructura/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <div class="container">
+        <p>Todos los derechos reservados by AgendSoft<br>Copyright © 2017</p>
+    </div>
+</footer>
---
+++ estructura/navbar.php
@@ -0,0 +1,63 @@
+
+
+  <nav class="navbar navbar-default  navbar-fixed-top">
+    <div class="container">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#"><i class="fa fa-home" aria-hidden="true"></i>
+          Agend Soft</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+
+      </ul>
+
+   <!-- Slider consulta, ingreso y edición de pacientess-->
+
+
+      <ul class="nav navbar-nav navbar-right">
+        <li class="dropdown mega-dropdown active">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-users" aria-hidden="true"></i>
+              Pacientes<span class="caret"></span></a>
+            <div class="dropdown-menu mega-dropdown-menu">
+                    <div class="container-fluid">
+                <!-- Tab panes -->
+                        <div class="tab-content">
+                          <div class="tab-pane active">
+                            <ul class="nav-list list-inline">
+                                <li><a href="#"><i class="fa fa-user fa-5x" aria-hidden="true"></i>
+                                  <i class="fa fa-search fa-2x" aria-hidden="true"></i>
+                                      <span>Buscarrr</span></a></li>
+                                <li><a href="registropaciente.php"><i class="fa fa-user-plus fa-5x" aria-hidden="true"></i>
+                                      <span>Agregar</span></a></li>
+                            </ul>
+                          </div>
+
+                        </div>
+                    </div>
+                  <!-- Nav tabs -->
+             </div>
+       </li>
+        <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user-circle-o" aria-hidden="true"></i>
+              Bienvenida, Jhoana <b class="caret"></b></a>
+            <ul class="dropdown-menu">
+                <li><a href="/user/preferences"><i class="icon-cog"></i> Preferences</a></li>
+                <li><a href="/help/support"><i class="icon-envelope"></i> Contact Support</a></li>
+                <li class="divider"></li>
+                <li><a href="/auth/logout"><i class="icon-off"></i> Cerrar Sesión</a></li>
+            </ul>
+        </li>
+     </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+</div>
